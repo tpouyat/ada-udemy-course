80b300af14864d4eff464f6d2adef8401bde553e
tancrede.pouyat <tancrede.pouyat@etu.hesge.ch>
2016-12-17 14:20:42 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ada-udemy-course
+This Udemy courses teaches you programming with Ada.
---
+++ Secret_Number.adb
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Secret_Number is
+		
+	begin
+end Secret_Number;
